f30f767455bc245ea9ee85667548bdc4540dcf50
harshmittal-cyber <mittalharsh4321@gmail.com>
2023-05-22 12:38:43 +0530

    added local variable and randomid resources

--- main.tf
+++ main.tf
@@ -2,3 +2,33 @@ resource "aws_s3_bucket" "s3bucket" {
   bucket= var.bucket
   tags= var.tags
 }
+
+resource "local_file" "file1"{
+    content="Harsh Mittal"
+    filename="harsh.html"
+}
+
+# used static content and localvariable filename
+resource "local_file" "file2"{
+    content="We need resources"
+    filename=local.a
+}
+
+# used random id with decimal and localvariable filename
+resource "local_file" "file3"{
+    content="${random_id.server.dec}"
+    filename=local.b
+}
+
+
+# locals
+locals{
+    a="harsh.py"
+    b="harsh1.js"
+}
+
+# random resource
+resource "random_id" "server"{
+  byte_length = 8
+}
+
